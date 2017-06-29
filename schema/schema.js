487b53cf7ee0e02c1698082847b36b2745df27f1
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-29 14:17:09 -0700

    fully connected between users and companies

--- schema/schema.js
+++ schema/schema.js
@@ -6,21 +6,29 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema // take in an rootquery and return a graphql schema instance
+  GraphQLSchema, // take in an rootquery and return a graphql schema instance
+  GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType), // collection of users to this company
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then( response => response.data);
+      }
+    }
+  })
 })
 
 const UserType = new GraphQLObjectType({
   name: 'User', // Capital first letter
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -36,14 +44,15 @@ const UserType = new GraphQLObjectType({
         .then(response => response.data);
       }
     }
-  }
+  })
 });
 
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
+
+    user: { // query to user type
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) { // go to database, look and drag the data
@@ -54,7 +63,17 @@ const RootQuery = new GraphQLObjectType({
         return axios.get(`http://localhost:3000/users/${args.id}`)
         .then(response => response.data)
       }
+    },
+
+    company: { // query to company
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(response => response.data);
+      }
     }
+
   }
 });
 
